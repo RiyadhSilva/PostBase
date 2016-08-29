205b69204a9eb861ac9fb049af41b1a45dc37bff
RiyadhSilva <riyadhlevi@gmail.com>
2016-08-29 16:09:46 -0300

    Criação do CardView
    Implementação dos métodos:
    - setCardElevation
    - setRadius

--- app/src/main/java/com/example/riyad/postbase/MainActivity.java
+++ app/src/main/java/com/example/riyad/postbase/MainActivity.java
@@ -2,16 +2,64 @@ package com.example.riyad.postbase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    public CardView cardView;
+    public SeekBar seekBar1;
+    public SeekBar seekBar2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CarroDB carrodb = new CarroDB(this);
+/*
+        Carro carro = new Carro();
+        carro.tipo="sedan";
+        carro.nome="fiat galaxy";
+        carro.desc = "Completao e economico";
+        carro.urlFoto = "X";
+        carro.urlInfo = "Y";
+        carro.urlVideo =  "Z";
+        carro.latitude="1";
+        carro.longitude="1";
+
+        carrodb.save(carro);
+*/
+
+        List<Carro> carros = carrodb.findAll();
+
+        for (Carro c: carros) {
+            System.out.println("id: " + c.id +" Nome: " + c.nome);
+        }
+
+        cardView = (CardView) findViewById(R.id.cardView);
+        seekBar1 = (SeekBar) findViewById(R.id.seekBar1);
+        seekBar2 = (SeekBar) findViewById(R.id.seekBar2);
     }
 
-    public void funcaoTeste (){
-        // atentendo a pedidos, alteração feita
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+        if(seekBar == this.seekBar1){
+            cardView.setCardElevation(progress); //elevacao
+        }else if (seekBar == this.seekBar2){
+            cardView.setRadius(progress); //Arrendodamento
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
