1612870536a130a346def35ba182f078a41d7d7a
RiyadhSilva <riyadhlevi@gmail.com>
2016-09-02 15:13:42 -0300

    Implementação de prioridade dos posts
    Início...

--- app/src/main/java/com/example/riyad/postbase/NovoPost.java
+++ app/src/main/java/com/example/riyad/postbase/NovoPost.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,6 +21,9 @@ import java.util.StringTokenizer;
 public class NovoPost extends AppCompatActivity {
     private EditText autor;
     private EditText desc;
+    private CheckBox prioridade_baixa;
+    private CheckBox prioridade_normal;
+    private CheckBox prioridade_alta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,9 @@ public class NovoPost extends AppCompatActivity {
 
         autor = (EditText) findViewById(R.id.novo_post_et_1);
         desc  = (EditText) findViewById(R.id.novo_post_et_2);
+        prioridade_baixa = (CheckBox) findViewById(R.id.novo_post_cb_baixo);
+        prioridade_normal = (CheckBox) findViewById(R.id.novo_post_cb_normal);
+        prioridade_alta = (CheckBox) findViewById(R.id.novo_post_cb_alto);
 
 
     }
@@ -49,11 +56,39 @@ public class NovoPost extends AppCompatActivity {
         Random gerador = new Random();
         int numero = gerador.nextInt(100);
         post.curtidas = String.valueOf(numero);
+        //Verifica a prioridade
+        boolean checked = ((CheckBox)view).isChecked();
+        //Verifica qual foi selecionada
+        switch (view.getId()){
+            case R.id.novo_post_cb_baixo:
+                if(checked){
+                    toast("Prioridade Baixa");
+                }else{
+
+                }
+                break;
+            case R.id.novo_post_cb_normal:
+                if(checked){
+                    toast("Prioridade Baixa");
+                }else{
+
+                }
+                break;
+            case R.id.novo_post_cb_alto:
+                if(checked){
+                    toast("Prioridade Alta");
+                }else{
+
+                }
+                break;
+        }
+
         //Inicia conexao com o Banco
         PostDB postDB = new PostDB(this);
         //Salva o post criado no banco
         postDB.save(post);
         toast("Postado com sucesso!");
+
         //Chama a Activity principal
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
--- app/src/main/java/com/example/riyad/postbase/SimpleAdapter.java
+++ app/src/main/java/com/example/riyad/postbase/SimpleAdapter.java
@@ -16,6 +16,7 @@ import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
