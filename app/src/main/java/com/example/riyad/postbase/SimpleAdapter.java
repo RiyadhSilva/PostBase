b5994191f63e594a3bd028ee587e4998e124d1bb
RiyadhSilva <riyadhlevi@gmail.com>
2016-09-26 02:06:32 -0300

    3ª Refatoração
    Refatoração de código na classe SimpleAdapter para uma melhor
    legibilidade.

--- app/src/main/java/com/example/riyad/postbase/SimpleAdapter.java
+++ app/src/main/java/com/example/riyad/postbase/SimpleAdapter.java
@@ -34,6 +34,18 @@ public class SimpleAdapter extends BaseAdapter{
     public Timer timer = null;
     //Um boolean para saber se a tarefa esta em execucao ou nao
     public boolean play = false;
+    private Animation a;
+    private CardView c;
+    private  TextView t;
+    private TextView p;
+    private TextView l;
+    private TextView d;
+    private int count;
+    private ImageButton bt_deletar;
+    private ImageButton bt_timer;
+    private SimpleDateFormat format;
+    private TimerTask tarefa;
+
     public SimpleAdapter(Context context){
         super();
         this.context = context; // O contexto é necessário para criar a view
@@ -44,6 +56,18 @@ public class SimpleAdapter extends BaseAdapter{
         List<Atividade> atividades = atividadeDB.findAll();
         return atividades;
 
+    }
+
+    public String nomeAtividade(int position){
+        String nome;
+        nome = atividades().get(atividades().size() - position - 1).nome;
+        return nome;
+    }
+
+    public String prioridadeAtividade(int position){
+        String prioridade;
+        prioridade = atividades().get(atividades().size() - position - 1).prioridade;
+        return prioridade;
     }
 
     @Override
@@ -62,8 +86,8 @@ public class SimpleAdapter extends BaseAdapter{
     @Override
     //Retorna a view para essa posição
     public View getView(final int position, View convertView, ViewGroup parent){
-        TextView t = new TextView(context);
-        final CardView c = new CardView(context);
+         t = new TextView(context);
+         c = new CardView(context);
 
         //Inicia o banco
         final AtividadeDB atividadeDB = new AtividadeDB(this.context);
@@ -77,14 +101,14 @@ public class SimpleAdapter extends BaseAdapter{
         t.setTextSize(15f);
 
         //Parte do TextView (Atividade)
-        TextView p = new TextView(context);
+        p = new TextView(context);
         p.setGravity(Gravity.CENTER_VERTICAL + Gravity.CENTER_HORIZONTAL);
         p.setText(atividades.get(position).desc);
         p.setTextColor(Color.parseColor("#FAE3C6"));
         p.setTextSize(20f);
 
         //Parte do TextView (Custo)
-        final TextView l = new TextView(context);
+        l = new TextView(context);
         l.setText(atividades.get(position).custo + " R$");
         l.setTextColor(Color.parseColor("#FAE3C6"));
         l.setTextSize(20f);
@@ -92,52 +116,71 @@ public class SimpleAdapter extends BaseAdapter{
         l.setX(l.getX() - 10);
 
         //Parte do TextView (Data)
-        final TextView d = new TextView(context);
+        d = new TextView(context);
         d.setTextColor(Color.parseColor("#FAE3C6"));
         d.setTextSize(10f);
         d.setGravity(Gravity.BOTTOM + Gravity.LEFT);
         d.setText(atividades.get(position).data);
 
         //Parte do Button (Deletar)
-        final ImageButton bt_deletar = new ImageButton(context);
+        bt_deletar = new ImageButton(context);
+        //Define o layout
+        loadBtDeletarLayout();
+        //Define a acao a ser tomada quando o botao for clicado
+        bt_deletar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Carrega a animacao
+                loadAnimation();
+                //Deleta o elemento na posicao
+                atividadeDB.delete(atividades.get(position));
+                //FeedBack
+                toast("Atividade excluída!");
+                //Inicia a animacao
+                c.startAnimation(a);
+            }
+        });
+
+        //Parte do Button (Timer)
+        bt_timer = new ImageButton(context);
+        //Define o layout do botao
+        loadBtTimerLayout();
+        //Define acao para quando o botao for clicado
+        bt_timer.setOnClickListener(new View.OnClickListener() {
+            Atividade atividade = new Atividade();
+            @Override
+            public void onClick(View v) {
+                loadTimer(position);
+
+            }
+        });
+
+        //Parte do CardView
+        count = 0;
+        if(prioridadeAtividade(position).equals("baixa")){
+            cardViewPrioridadeBaixa();
+        }else if (prioridadeAtividade(position).equals("normal")){
+            cardViewPrioridadeNormal();
+        } else if (prioridadeAtividade(position).equals("alta")){
+            cardViewPrioridadeAlta();
+        }
+        //Carrega o cardView
+        loadCardView();
+        return c;
+    }
+
+    private void loadBtDeletarLayout() {
         bt_deletar.setX(0f);
         bt_deletar.setY(0f);
         bt_deletar.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams
                 .WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
         bt_deletar.setImageResource(R.drawable.ic_delete);
-        bt_deletar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean show = true; //Mostra ou não a view
-                int angulo = 180;
-                ScaleAnimation encolher = new ScaleAnimation(
-                        1.0f, 0.0f,
-                        1.0f, 0.0f,
-                        Animation.RELATIVE_TO_SELF, 0.5f,
-                        Animation.RELATIVE_TO_SELF, 0.5f
-                );
-                ScaleAnimation aumentar = new ScaleAnimation(
-                        0.0f, 1.0f,
-                        0.0f, 1.0f,
-                        Animation.RELATIVE_TO_SELF, 0.5f,
-                        Animation.RELATIVE_TO_SELF, 0.5f
-                );
-                Animation a = show ? encolher : aumentar;
-                a.setDuration(1000);
-                a.setFillAfter(true);
-                //Deleta o elemento na posicao
-                atividadeDB.delete(atividades.get(position));
-                //
-                toast("Atividade excluída!");
-                c.startAnimation(a);
-            }
-        });
+    }
 
-        //Parte do Button (Timer)
-        final ImageButton bt_timer = new ImageButton(context);
-        //Display para pegar o tamanho da tela
-        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+    private void loadBtTimerLayout() {
+        WindowManager windowManager = (WindowManager) context.getSystemService
+                (Context.WINDOW_SERVICE);
         Display display = windowManager.getDefaultDisplay();
         int largura = display.getWidth() - 105;
         bt_timer.setX(largura);
@@ -145,73 +188,50 @@ public class SimpleAdapter extends BaseAdapter{
         bt_timer.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams
                 .WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
         bt_timer.setImageResource(R.drawable.ic_action_play);
-        bt_timer.setOnClickListener(new View.OnClickListener() {
-            Atividade atividade = new Atividade();
-            int  valor_atual;
-            @Override
-            public void onClick(View v) {
-                final SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-                Timer ini;
-                if(timer == null || !play){
-                    play = true;
-                    bt_timer.setImageResource(R.drawable.ic_action_stop);
-                    timer = new Timer();
-                    ini = new Timer();
-                    TimerTask tarefa = new TimerTask() {
-                        @Override
-                        public void run() {
-                            try {
-                                System.out.println("Hora: " + format.format(new Date().getTime()));
-                            }catch (Exception e){
-                                e.printStackTrace();
-                            }
-                        }
-                    };
+    }
 
-                    timer.scheduleAtFixedRate(tarefa, 0, 1000);
-                    toast("Tarefa iniciada!");
-                    //Notificacao
-                    notificacao("Atividade iniciada!", "A atividade " + atividades().get(position).nome + " foi iniciada!");
-
-                }else if(play == true){
-                    play = false;
-                    bt_timer.setImageResource(R.drawable.ic_action_play);
-                    toast("Tarefa encerrada!");
-                    timer.cancel();
-                    //Envia um e-mail para o administrador
-                    Intent emailIntent = new Intent(Intent.ACTION_VIEW);
-                    Uri data = Uri.parse("mailto:?subject=" + "blah blah subject" + "&body=" + "blah blah body" + "&to=" + "riyadhlevi@gmail.com");
-                    emailIntent.setData(data);
-                    context.startActivity(emailIntent);
-                    //Notificacao
-                    notificacao("Atividade finalizada!", "A atividade " + atividades().get(position).nome + " foi finalizada!");
+    private void loadTimer(int position) {
+        format = new SimpleDateFormat("HH:mm:ss");
+        if(timer == null || !play){
+            play = true;
+            bt_timer.setImageResource(R.drawable.ic_action_stop);
+            timer = new Timer();
+            tarefa = new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("Hora: " + format.format(new Date().getTime()));
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
                 }
+            };
 
-            }
-        });
+            timer.scheduleAtFixedRate(tarefa, 0, 1000);
+            toast("Tarefa iniciada!");
+            //Notificacao
+            notificacao("Atividade iniciada!", "A atividade " + nomeAtividade(position) + " " +
+                    "foi iniciada!");
 
-        //Parte do CardView
-        int count = 0;
-        if(atividades().get(atividades().size() - position - 1).prioridade.equals("baixa")){
-            c.setCardBackgroundColor(Color.parseColor("#EFEFEF"));
-            t.setTextColor(Color.parseColor("#463239"));
-            p.setTextColor(Color.parseColor("#463239"));
-            l.setTextColor(Color.parseColor("#463239"));
-            d.setTextColor(Color.parseColor("#463239"));
-        }else if (atividades().get(atividades().size() - position - 1).prioridade.equals("normal")){
-            c.setCardBackgroundColor(Color.parseColor("#CAEBF2"));
-            t.setTextColor(Color.parseColor("#254D32"));
-            p.setTextColor(Color.parseColor("#254D32"));
-            l.setTextColor(Color.parseColor("#254D32"));
-            d.setTextColor(Color.parseColor("#254D32"));
-        } else if (atividades().get(atividades().size() - position - 1).prioridade.equals("alta")){
-            c.setCardBackgroundColor(Color.parseColor("#FF3B3F"));
-            t.setTextColor(Color.parseColor("#D0DB97"));
-            p.setTextColor(Color.parseColor("#D0DB97"));
-            l.setTextColor(Color.parseColor("#D0DB97"));
-            d.setTextColor(Color.parseColor("#D0DB97"));
+        }else if(play == true){
+            play = false;
+            bt_timer.setImageResource(R.drawable.ic_action_play);
+            toast("Tarefa encerrada!");
+            timer.cancel();
+            //Envia um e-mail para o administrador
+            Intent emailIntent = new Intent(Intent.ACTION_VIEW);
+            Uri data = Uri.parse("mailto:?subject=" + "Organize - Atividade finalizada!" + "&body="
+                    + "A atividade " + nomeAtividade(position) + " foi finalizada!" + "&to=" +
+                    "riyadhlevi@gmail.com");
+            emailIntent.setData(data);
+            context.startActivity(emailIntent);
+            //Notificacao
+            notificacao("Atividade finalizada!", "A atividade " + nomeAtividade(position) + " foi " +
+                    "finalizada!");
         }
+    }
 
+    private void loadCardView() {
         c.setRadius(5f);
         c.setUseCompatPadding(true);
         c.setMinimumHeight(100);
@@ -223,11 +243,50 @@ public class SimpleAdapter extends BaseAdapter{
         c.addView(d, count);
         c.addView(l, count);
         count++;
-
-        return c;
     }
 
-    private void bt_timerOnClick() {
+    private void cardViewPrioridadeAlta() {
+        c.setCardBackgroundColor(Color.parseColor("#FF3B3F"));
+        t.setTextColor(Color.parseColor("#D0DB97"));
+        p.setTextColor(Color.parseColor("#D0DB97"));
+        l.setTextColor(Color.parseColor("#D0DB97"));
+        d.setTextColor(Color.parseColor("#D0DB97"));
+    }
+
+    private void cardViewPrioridadeNormal() {
+        c.setCardBackgroundColor(Color.parseColor("#CAEBF2"));
+        t.setTextColor(Color.parseColor("#254D32"));
+        p.setTextColor(Color.parseColor("#254D32"));
+        l.setTextColor(Color.parseColor("#254D32"));
+        d.setTextColor(Color.parseColor("#254D32"));
+    }
+
+    private void cardViewPrioridadeBaixa() {
+        c.setCardBackgroundColor(Color.parseColor("#EFEFEF"));
+        t.setTextColor(Color.parseColor("#463239"));
+        p.setTextColor(Color.parseColor("#463239"));
+        l.setTextColor(Color.parseColor("#463239"));
+        d.setTextColor(Color.parseColor("#463239"));
+    }
+
+    private void loadAnimation() {
+        boolean show = true; //Mostra ou não a view
+        int angulo = 180;
+        ScaleAnimation encolher = new ScaleAnimation(
+                1.0f, 0.0f,
+                1.0f, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        );
+        ScaleAnimation aumentar = new ScaleAnimation(
+                0.0f, 1.0f,
+                0.0f, 1.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        );
+        a = show ? encolher : aumentar;
+        a.setDuration(1000);
+        a.setFillAfter(true);
     }
 
     private void toast(String msg){
