87eb379f59cbeba3ee6a346e276c365f733b52a7
RiyadhSilva <riyadhlevi@gmail.com>
2016-09-26 14:45:54 -0300

    Implementação  - Gráficos (ActionBar e ajustes)
    Implementação de uma ActionBar e seus respectivos métodos para a tela
    referente aos gráficos de custos.

--- app/src/main/java/com/example/riyad/postbase/GraphActivity.java
+++ app/src/main/java/com/example/riyad/postbase/GraphActivity.java
@@ -1,8 +1,12 @@
 package com.example.riyad.postbase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.BarGraphSeries;
@@ -28,6 +32,15 @@ public class GraphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        //Define o título da actionBar
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.drawable.ic_action_graph);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setTitle("Gráficos");
+        actionBar.setDisplayShowTitleEnabled(true);
+        //Ativa a exibicao do icone na action bar
+        actionBar.setDisplayShowHomeEnabled(true);
 
         valorTotal = (TextView) findViewById(R.id.graph_tv_1);
         numAtividades = (TextView) findViewById(R.id.graph_tv_2);
@@ -78,4 +91,55 @@ public class GraphActivity extends AppCompatActivity {
         this.dados[this.position] = atual;
         position++;
     }
+
+    //ActionBar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //Infla o menu com os botoes da actionbar
+        getMenuInflater().inflate(R.menu.menu_graph, menu);
+        return true;
+    }
+
+    //ActionBar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_new){
+            Intent intent = new Intent(this, NovaAtividade.class);
+            startActivity(intent);
+            return true;
+        }else if (id == R.id.action_search){
+            Intent i = new Intent(this, PesquisarActivity.class);
+            startActivity(i);
+            return true;
+        } else if (id == R.id.action_refresh){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_delete){
+            AtividadeDB atividadeDB = new AtividadeDB(this);
+            List<Atividade> atividades = atividadeDB.findAll();
+            for (Atividade p:
+                    atividades) {
+                atividadeDB.delete(p);
+            }
+            toast("Todos os atividades foram deletados!");
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+            return true;
+        } else if (id == R.id.action_graph){
+            Intent intent = new Intent(this, GraphActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toast(String msg){
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
